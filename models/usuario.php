9e85fcea24213963f0eabb2e03fabce02622378b
Pedro Paulo Magno <ppfmagno@gmail.com>
2019-03-27 16:28:25 -0300

    Adição do atributo de senha

--- models/usuario.php
+++ models/usuario.php
@@ -4,23 +4,25 @@ class Usuario {
   public $id;
   public $nome;
   public $login;
+  public $senha;
   public $email;
   public $crea;
   public $endereco;
   public $inventarios;
 
-  function __construct($id, $nome, $login, $email, $crea, $endereco, $inventarios) {
+  function __construct($id, $nome, $login, $email, $senha, $crea, $endereco, $inventarios) {
     $this->id = $id;
     $this->nome = $nome;
     $this->login = $login;
     $this->email = $email;
+    $this->senha = $senha;
     $this->crea = $crea;
     $this->endereco = $endereco;
     $this->inventarios = $inventarios;
   }
 
   public static function newEmpty() {
-    $instance = new self(null, null, null, null, null, null, null);
+    $instance = new self(null, null, null, null, null, null, null, null);
     return $instance;
   }
 }
