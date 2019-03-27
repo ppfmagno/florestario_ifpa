116fdd0d9269dcfe0497532b1b7b3e1667567667
Pedro Paulo Magno <ppfmagno@gmail.com>
2019-03-27 18:08:50 -0300

    Criação de um teste para inserir um usuário

--- index.php
+++ index.php
@@ -1,12 +1,33 @@
 <?php
-// echo phpinfo();
-require_once './models/dbcontrole.php';
-$config = parse_ini_file('./config.ini');
-$con = new DBcontrole($config);
-$usuarios = $con->getUsuarios();
-print_r($usuarios);
-?>
+require_once './models/estado.php';
+require_once './models/municipio.php';
+require_once './models/endereco.php';
+require_once './models/usuario.php';
+$us = Usuario::newEmpty();
+$us->nome = 'John Doe';
+$us->login = 'johndoe78987';
+$us->email = 'jhondoe123@j.doe';
+$us->senha = '1345';
+$us->crea = '78864564';
+$us->endereco = Endereco::newEmpty();
+$us->endereco->rua = 'Rua Longe';
+$us->endereco->cep = '13245678';
+$us->endereco->numero = 456;
+$us->endereco->municipio = Municipio::newEmpty();
+$us->endereco->municipio->nome = 'Belém';
+$us->endereco->municipio->estado = Estado::newEmpty();
+$us->endereco->municipio->estado->nome = 'Pará';
 
+require_once './models/dbcontrole.php';
+$dbconfig = parse_ini_file('./dbconfig.ini');
+$con = new DBcontrole($dbconfig);
+
+// $estado = Estado::newEmpty();
+// $estado->nome = 'Acre';
+
+$con->insertUsuario($us);
+
+?>
 <?php include_once './views/partials/header.php'; ?>
 
   <ul>
